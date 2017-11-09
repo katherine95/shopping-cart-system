c804f097b71397a1b5566e8b48827ff74085f5ee
katherine95 <kathiekim95@gmail.com>
2017-11-09 10:57:18 +0300

    updated

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -1,4 +1,14 @@
 require 'test_helper'
+test "should create line_item" do
+  assert_difference('LineItem.count') do
+    post line_items_url, params: { product_id: products(:ruby).id }
+  end
+
+  follow_redirect!
+
+  assert_select 'h2', 'Your Pragmatic Cart'
+  assert_select 'li', 'Programming Ruby 1.9'
+end
 
 class LineItemsControllerTest < ActionDispatch::IntegrationTest
   setup do
